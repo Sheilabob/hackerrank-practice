46024803fb068f7a26a88fc49b255c374bc427c4
Sheila Strahan <smstrahan@gmail.com>
2022-07-22 19:20:48 -0600

    divisiable sum pairs

--- breaking_the_records.py
+++ breaking_the_records.py
@@ -43,3 +43,18 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
+
+# Youtube solution:
+
+# def breakingRecords(scores):
+#     mincount = maxcount = 0
+#     minscore = maxscore = scores[0]
+
+#     for i in range(1, len(scores)):
+#         if minscore < scores[i]:
+#             minscore = scores[i]
+#             mincount += 1
+#         elif maxscore > scores[i]:
+#             maxscore = scores[i]
+#             maxcount += 1
+#     return mincount, maxcount
