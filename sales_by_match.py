2b01a9f729891bfb0058448aa2edfe9160d684f6
Sheila Strahan <smstrahan@gmail.com>
2022-09-06 18:49:06 -0600

    sales by match

---
+++ sales_by_match.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    holder = []
+    pair_count = 0
+    for i in range(n):
+        if ar[i] not in holder:
+            new_pairs = (ar.count(ar[i])//2)
+            pair_count += new_pairs
+        holder.append(ar[i])
+    return pair_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
